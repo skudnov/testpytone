44c51684a0b250d2d5afbc178aacf52501647dfc
skudnov <skudnoff.a@yandex.ru>
2017-12-06 00:48:08 +0300

    Добавил хвост ,но не сделал реализацию,надо как то перепределять блок либо делать массив

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ WIDTH = 600
 # Высота
 HEIGHT = 400
 # Размер головы
-OVAL = 10
+SIZ = 10
 # Проверка на игру
 IN_GAME = True
+StartGame = True
 
 """Обработка всей программы"""
 
 
 def mains():
     global IN_GAME
+    global StartGame
     if IN_GAME:
-
         s.move()
-
-        head = c.coords(s.cell[-1].instance)
-        x1, y1, x2, y2 = head
+        tail = 0, 0, 0, 0
+        head_coords = c.coords(s.instance)
+        x1, y1, x2, y2 = head_coords
+        if StartGame:
+            Platform()
+            StartGame = False
         if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
             IN_GAME = False
-        root.after(200, mains)
+        if head_coords == c.coords(BLOCK):
+            tail = head_coords
+        else:
+            create_tail(tail)
+        root.after(100, mains)
     else:
         c.create_text(WIDTH / 2, HEIGHT / 2,
                       text="Игра окончена!",
@@ -31,17 +39,12 @@ def mains():
                       fill="red")
 
 
-""" Отрисовка головы"""
-
-
-class Cell(object):
-
-    def __init__(self):
-        posx = OVAL * random.randint(1, (WIDTH - OVAL) / OVAL)
-        posy = OVAL * random.randint(1, (HEIGHT - OVAL) / OVAL)
-        self.instance = c.create_oval(posx, posy,
-                                      posx + OVAL, posy + OVAL,
-                                      fill="white")
+def create_tail(head):
+    global TAIL
+    x1, y1, x2, y2 = head
+    TAIL = c.create_oval(x1, y1,
+                         x2, y2,
+                         fill="red")
 
 
 """Отрисовка ячейки платформы"""
@@ -49,36 +52,67 @@ class Cell(object):
 
 class Platform(object):
     def __init__(self):
-        posx = OVAL * random.randint(1, (WIDTH - OVAL) / OVAL)
-        posy = OVAL * random.randint(1, (HEIGHT - OVAL) / OVAL)
-        self.instance = c.create_rectangle(posx, posy,
-                                           posx + OVAL, posy + OVAL,
+        global BLOCK
+        i = 0
+        g = 0
+        pl = c.coords(s.instance)
+        x1, y1, x2, y2 = pl
+        for i in range(0, 9):
+            if 0 <= i <= 2:
+                BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
                                            fill="green")
-
+                g = g + 10
+            elif 3 <= i <= 5:
+                if i == 3:
+                    BLOCK = c.create_rectangle(x1, y1 + 10, x2, y2 + 10,
+                                               fill="green")
+                    y1 += 10
+                    y2 += 10
+                    g = 0
+                else:
+                    g = g + 10
+                    BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
+                                               fill="green")
+            elif 6 <= i <= 8:
+                if i == 6:
+                    y1 += 10
+                    y2 += 10
+                    BLOCK = c.create_rectangle(x1, y1, x2, y2,
+                                               fill="green")
+                    g = 0
+                else:
+                    g = g + 10
+                    BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
+                                               fill="green")
 
 
 """Класс головы который отвечает за направление движения и за передвижение"""
 
 
 class Head(object):
-    def __init__(self, cell):
-        self.cell = cell
+    def __init__(self):
+        global HEAD
         self.mapping = {"Down": (0, 1), "Right": (1, 0),
                         "Up": (0, -1), "Left": (-1, 0), "None": (0, 0)}
         # initial movement direction
         self.vector = self.mapping["None"]
 
+        posx = SIZ * random.randint(1, (WIDTH-50 - SIZ) / SIZ)
+        posy = SIZ * random.randint(1, (HEIGHT-50 - SIZ) / SIZ)
+        self.instance = c.create_oval(posx, posy,
+                                      posx + SIZ, posy + SIZ,
+                                      fill="White")
+
     def move(self):
         """ перемещает голову с указаным вектором"""
-        for index in range(len(self.cell)):
-            cel = self.cell[index].instance
+        cel = s.instance
 
-            x1, y1, x2, y2 = c.coords(self.cell[index].instance)
-            if self.mapping == "None":
-                c.coords(cel, x1, y1, x2, y2)
-            else:
-                c.coords(cel, x1 + self.vector[0] * OVAL, y1 + self.vector[1] * OVAL,
-                         x2 + self.vector[0] * OVAL, y2 + self.vector[1] * OVAL)
+        x1, y1, x2, y2 = c.coords(s.instance)
+        if self.mapping == "None":
+            c.coords(cel, x1, y1, x2, y2)
+        else:
+            c.coords(cel, x1 + self.vector[0] * SIZ, y1 + self.vector[1] * SIZ,
+                     x2 + self.vector[0] * SIZ, y2 + self.vector[1] * SIZ)
 
     def change_direction(self, event):
         """ изменяет направление головы """
@@ -94,10 +128,7 @@ root.title("Test")
 c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#003330")
 c.grid()
 c.focus_set()
-
-plat = [Platform()]
-cell = [Cell()]
-s = Head(cell)
+s = Head()
 c.bind('<KeyPress>', s.change_direction)
 mains()
 root.mainloop()
