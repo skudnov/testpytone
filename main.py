59935835ab72a314f7452e727b75073a093de2f5
skudnov <skudnoff.a@yandex.ru>
2017-12-09 02:39:47 +0300

    Сделал все что мог ,надо сделать заполнени вырезанной площади

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ SIZ = 10
 # Проверка на игру
 IN_GAME = True
 StartGame = True
+blocklist = []
 
 """Обработка всей программы"""
 
@@ -19,18 +20,16 @@ def mains():
     global StartGame
     if IN_GAME:
         s.move()
-        tail = 0, 0, 0, 0
         head_coords = c.coords(s.instance)
         x1, y1, x2, y2 = head_coords
         if StartGame:
             Platform()
             StartGame = False
+
+        if head_coords not in blocklist:
+            create_tail(head_coords)
         if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
             IN_GAME = False
-        if head_coords == c.coords(BLOCK):
-            tail = head_coords
-        else:
-            create_tail(tail)
         root.after(100, mains)
     else:
         c.create_text(WIDTH / 2, HEIGHT / 2,
@@ -44,7 +43,7 @@ def create_tail(head):
     x1, y1, x2, y2 = head
     TAIL = c.create_oval(x1, y1,
                          x2, y2,
-                         fill="red")
+                         fill="White")
 
 
 """Отрисовка ячейки платформы"""
@@ -52,38 +51,38 @@ def create_tail(head):
 
 class Platform(object):
     def __init__(self):
-        global BLOCK
-        i = 0
         g = 0
         pl = c.coords(s.instance)
         x1, y1, x2, y2 = pl
         for i in range(0, 9):
             if 0 <= i <= 2:
-                BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
-                                           fill="green")
+
+                blocklist.append(c.coords(c.create_rectangle(x1 + g, y1, x2 + g, y2,
+                                   fill="green")))
                 g = g + 10
             elif 3 <= i <= 5:
                 if i == 3:
-                    BLOCK = c.create_rectangle(x1, y1 + 10, x2, y2 + 10,
-                                               fill="green")
+                    blocklist.append(c.coords(c.create_rectangle(x1, y1 + 10, x2, y2 + 10,
+                                       fill="green")))
                     y1 += 10
                     y2 += 10
                     g = 0
                 else:
                     g = g + 10
-                    BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
-                                               fill="green")
+                    blocklist.append(c.coords(c.create_rectangle(x1 + g, y1, x2 + g, y2,
+                                       fill="green")))
+
             elif 6 <= i <= 8:
                 if i == 6:
                     y1 += 10
                     y2 += 10
-                    BLOCK = c.create_rectangle(x1, y1, x2, y2,
-                                               fill="green")
+                    blocklist.append(c.coords(c.create_rectangle(x1, y1, x2, y2,
+                                       fill="green")))
                     g = 0
                 else:
                     g = g + 10
-                    BLOCK = c.create_rectangle(x1 + g, y1, x2 + g, y2,
-                                               fill="green")
+                    blocklist.append(c.coords(c.create_rectangle(x1 + g, y1, x2 + g, y2,
+                                       fill="green")))
 
 
 """Класс головы который отвечает за направление движения и за передвижение"""
@@ -95,10 +94,10 @@ class Head(object):
         self.mapping = {"Down": (0, 1), "Right": (1, 0),
                         "Up": (0, -1), "Left": (-1, 0), "None": (0, 0)}
         # initial movement direction
-        self.vector = self.mapping["None"]
+        self.vector = self.mapping["Right"]
 
-        posx = SIZ * random.randint(1, (WIDTH-50 - SIZ) / SIZ)
-        posy = SIZ * random.randint(1, (HEIGHT-50 - SIZ) / SIZ)
+        posx = SIZ * random.randint(1, (WIDTH - 50 - SIZ) / SIZ)
+        posy = SIZ * random.randint(1, (HEIGHT - 50 - SIZ) / SIZ)
         self.instance = c.create_oval(posx, posy,
                                       posx + SIZ, posy + SIZ,
                                       fill="White")
